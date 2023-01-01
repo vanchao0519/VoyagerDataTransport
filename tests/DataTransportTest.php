1b2d196b6be26e4128775e48420e55d9bf8c0d9a
vanchao0519 <vanchao0519@gmail.com>
2023-01-01 19:04:45 +0800

    Add test case

---
+++ tests/DataTransportTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace VoyagerDataTransport\Test;
+
+use VoyagerDataTransport\Console\Commands\Traits\VoyagerDataController;
+
+class DataTransportTest extends \PHPUnit\Framework\TestCase {
+
+    use VoyagerDataController;
+
+    private $_controllerNamePre = "Import";
+
+    private $_tableName = "posts";
+
+    private function _tableNameToControllerName (string $tableName): string
+    {
+        $tableName = strtolower($tableName);
+        $tempArr = explode("_", $tableName);
+        $baseName = '';
+
+        foreach ($tempArr as $s) {
+            $baseName .= ucfirst($s);
+        }
+        
+        return $baseName;
+    }
+
+    private function _expectedControllerName (): string
+    {
+        return "{$this->_controllerNamePre}{$this->_tableNameToControllerName($this->_tableName)}";
+    }
+
+	public function test_getControllerName() {
+        $expected = $this->_expectedControllerName();
+        $actual = $this->getControllerName($this->_tableName);
+        $this->assertEquals($expected, $actual);
+	}
+
+}
