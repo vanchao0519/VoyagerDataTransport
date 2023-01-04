4ccc7901e5aa49fb30d15e4297735b44645aec3e
vanchao0519 <vanchao0519@gmail.com>
2023-01-04 19:25:31 +0800

    Refactor

---
+++ src/Console/Commands/Traits/VoyagerDataCommon.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace VoyagerDataTransport\Console\Commands\Traits;
+
+trait VoyagerDataCommon
+{
+
+     /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+    protected function resolveStubPath($stub)
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : dirname(__DIR__, 1).$stub;
+    }
+
+    /**
+     * Get the desired class name from the input.
+     *
+     * @return string
+     */
+    protected function getNameInput()
+    {
+        return trim($this->argument('tableName'));
+    }
+
+}
--- src/Console/Commands/Traits/VoyagerDataView.php
+++ src/Console/Commands/Traits/VoyagerDataView.php
@@ -4,30 +4,6 @@ namespace VoyagerDataTransport\Console\Commands\Traits;
 
 trait VoyagerDataView
 {
- 
-     /**
-     * Resolve the fully-qualified path to the stub.
-     *
-     * @param  string  $stub
-     * @return string
-     */
-    protected function resolveStubPath($stub)
-    {
-        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
-            ? $customPath
-            : dirname(__DIR__, 1).$stub;
-    }
-
-    /**
-     * Get the desired class name from the input.
-     *
-     * @return string
-     */
-    protected function getNameInput()
-    {
-        return trim($this->argument('tableName'));
-    }
-
     /**
      * Replace the view name for the given stub.
      *
