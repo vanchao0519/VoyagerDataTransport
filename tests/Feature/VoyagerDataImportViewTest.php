fd51e2bf33d0e8948c05c0f18adc4600467e8b21
vanchao0519 <vanchao0519@gmail.com>
2023-01-14 09:07:10 +0800

    Add data import view create test case

---
+++ tests/Feature/VoyagerDataImportViewTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\Feature\Traits\ParameterTrait;
+use Tests\TestCase;
+use VoyagerDataTransport\Contracts\ICommandStatus;
+
+class VoyagerDataImportViewTest extends TestCase implements ICommandStatus
+{
+    use ParameterTrait;
+
+    /**
+     * Confirm that create browse view command is worked.
+     *
+     * @return void
+     */
+    public function test_command(): void
+    {
+
+        $this->artisan("voyager:data:transport:import-data:view {$this->_getTableName()}")
+            ->assertExitCode(self::ALL_PROCESS_SUCCESS_CODE);
+    }
+
+    public function test_is_file_created ()
+    {
+        $this->assertFileExists($this->_getFile());
+    }
+
+    private function _getFile (): string
+    {
+        $file = "resources/views/vendor/voyager/{$this->_getTableName()}/import-data.blade.php";
+        return $file;
+    }
+}
